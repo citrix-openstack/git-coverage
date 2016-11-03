322661b92c2478ec4818be58d870046b4572b745
John Hua <john.hua@citrix.com>
2016-11-03 13:45:54 +0800

    Fix dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='Git-Coverage',
-      version='1.0',
-      requires=['coverage'],
+      version='1.0.1',
+      install_requires=['coverage'],
       scripts=['git-coverage']
       )

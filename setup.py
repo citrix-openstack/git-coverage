334403ca9b31f2bac9dc54b88492b8e6345fbf0a
John Hua <john.hua@citrix.com>
2016-11-16 10:27:21 +0800

    Fix coveragerc option reading
    ConfigParser's get function is not like dict.get() which can use a parameter as
    defaults.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='Git-Coverage',
-      version='1.0.1',
+      version='1.0.2',
       install_requires=['coverage'],
       scripts=['git-coverage']
       )
